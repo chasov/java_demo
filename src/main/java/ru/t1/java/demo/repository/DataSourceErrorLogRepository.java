2a273da861edd0ef85577de187dc784084c64da9
Mike Boychook <mikegti@yandex.com>
2024-10-31 20:53:29 +0300

    Added Repositories Interfaces

---
+++ src/main/java/ru/t1/java/demo/repository/DataSourceErrorLogRepository.java
@@ -0,0 +1,12 @@
+package ru.t1.java.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.t1.java.demo.model.DataSourceErrorLog;
+import ru.t1.java.demo.model.Transaction;
+
+import java.util.Optional;
+
+public interface DataSourceErrorLogRepository extends JpaRepository<DataSourceErrorLog, Long> {
+    @Override
+    Optional<DataSourceErrorLog> findById(Long aLong);
+}
