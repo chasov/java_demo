39abd552f750de1839e168612eefc37468c4e34c
Polina Pashkovskaya <polina.pashkovskaya@NetCracker.com>
2024-11-02 12:11:32 +0300

    delete extra imports

--- src/main/java/ru/t1/java/demo/T1JavaDemoApplication.java
+++ src/main/java/ru/t1/java/demo/T1JavaDemoApplication.java
@@ -3,7 +3,6 @@ package ru.t1.java.demo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @Slf4j
