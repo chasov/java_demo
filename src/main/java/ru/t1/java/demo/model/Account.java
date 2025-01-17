36c9c579de3875485b8c8b8143ff6eaf8bf24e85
Nikita <kissesksk@gmail.com>
2025-01-17 17:43:47 +0300

    t1 homework refactoring + create model and add migration

---
+++ src/main/java/ru/t1/java/demo/model/Account.java
@@ -0,0 +1,27 @@
+package ru.t1.java.demo.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.jpa.domain.AbstractPersistable;
+import ru.t1.java.demo.model.enums.AccountType;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "account")
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "account_type")
+    private AccountType accountType;
+    @Column(name = "balance")
+    private BigDecimal balance;
+}
---
+++ src/main/java/ru/t1/java/demo/model/DataSourceErrorLog.java
@@ -0,0 +1,24 @@
+package ru.t1.java.demo.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "data_source_error_log")
+public class DataSourceErrorLog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    @Column(name = "exception_stacktrace")
+    private String exceptionStackTrace;
+    @Column(name = "error_message")
+    private String errorMessage;
+    @Column(name = "method_signature")
+    private String methodSignature;
+}
