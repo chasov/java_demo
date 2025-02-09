4440b2eefb4afdc5b7ae5eaae77523a3ed69a471
Igor <dmitriev.igor91@mail.ru>
2025-02-09 14:47:48 +0300

    выполнение третьего задания

--- service_number_one/src/main/resources/db/changelog/2025/20-01-changelog-03.sql
+++ service_number_one/src/main/resources/db/changelog/2025/20-01-changelog-03.sql
@@ -4,10 +4,10 @@ CREATE TABLE account
 (
     account_id     BIGINT         NOT NULL DEFAULT NEXTVAL('account_seq'),
     client_id      BIGINT         NOT NULL,
-    account_type   VARCHAR(32)   NOT NULL,
-    account_status VARCHAR(32)   NOT NULL,
+    account_type   VARCHAR(32)    NOT NULL,
+    account_status VARCHAR(32)    NOT NULL,
     balance        DECIMAL(18, 2) NOT NULL,
-    frozen_amount  BOOLEAN        NOT NULL DEFAULT FALSE,
+    frozen_amount  DECIMAL(18, 2) NOT NULL CHECK (frozen_amount >= 0),
     CONSTRAINT pk_account PRIMARY KEY (account_id),
     CONSTRAINT fk_client FOREIGN KEY (client_id) REFERENCES client (client_id)
 );
